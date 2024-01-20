27cc2113b2ce979a62ce43d65086ed5038775ec2
rakshanda242004 <143547338+rakshanda242004@users.noreply.github.com>
2024-01-20 08:19:44 +0530

    Add files via upload

---
+++ JAVA/pairs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pairs {
+public static void print_pairs(int numbers[]) {
+    for (int i=0; i<numbers.length; i++) {
+        int current = numbers[i];
+        for(int j=i+1; j<numbers.length; j++) {
+            System.out.print("(" + current +"," + numbers[j] + ")");
+        }
+        System.out.println();
+    }
+}
+
+public static void main( String args[]) {
+    int numbers[] = {2, 4, 6, 8, 10};
+    print_pairs(numbers);
+}
+}
